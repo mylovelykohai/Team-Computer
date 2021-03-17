cc5f6a4b3ae023112cf11234887c51ec52f09354
Dec <fluffyjeb@gmail.com>
2021-03-17 16:37:57 +0000

    Basic implementation of networking
    Currently will connect to the server and send messages, the UI will not update for received messages, however it will come up within the console.
    
    When you download, make sure to update the ip address within Message_Conversation.java

--- Application/app/src/main/java/com/example/messagenofrag/Message_Conversation.java
+++ Application/app/src/main/java/com/example/messagenofrag/Message_Conversation.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,12 +18,21 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class Message_Conversation extends AppCompatActivity {
     private RecyclerView mUsers_RecyclerView;
     private List<User> mUsers;
+
+    connectionThread connection = new connectionThread("androidAPP","127.0.0.1",1200);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,13 +44,17 @@ public class Message_Conversation extends AppCompatActivity {
         EditText mEdit = (EditText) findViewById((R.id.theEditText));
         configureBackBtn();
         configureSend(mEdit);
+
+        connection.start();
     }
+
     class UsersAdapter extends RecyclerView.Adapter<UserViewHolder>{
         private List<User> mUsers;
         public UsersAdapter(List<User> users) {
             super();
             this.mUsers = users;
         }
+
 
         @NonNull
         @Override
@@ -73,21 +87,35 @@ public class Message_Conversation extends AppCompatActivity {
     }
     private void configureBackBtn(){
         Button button = (Button) findViewById(R.id.btn_bck);
-        button.setOnClickListener(new View.OnClickListener(){
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                startActivity(new Intent(Message_Conversation.this, MainActivity.class));
+            public void onClick(View view) {
+                finish();
             }
         });
     }
+
+    public void onDetachedFromWindow()
+    {
+        super.onDetachedFromWindow();
+        Log.d("onDestory","called");
+        connection.endConnection();
+    }
+
+
     private void configureSend(EditText mEdit){
         Button button = (Button) findViewById(R.id.btn_send);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Log.d("EditText ", mEdit.getText().toString());
                 //mEdit.getText().toString() is the value held within the EditText box
                 //Find a way to make the onClick method (This one) send the contents of mEdit to the database.
+                String message = mEdit.getText().toString();
+
+                messageObject.updateMessage(message);
+                connection.sendMessage(message);
+                mEdit.setText("", TextView.BufferType.EDITABLE);
+
             }
         });
     }
@@ -110,5 +138,166 @@ class User{
 
     public String getmEmotion() {
         return mEmotion;
+    }
+
+
+}
+class connectionThread extends Thread
+{
+    int PORT;
+    String IPADDR;
+
+    String username;
+
+    String message;
+    String recievedMessage;
+    String lastSentMessage;
+    String lastRecievedMessage;
+    Boolean socketStatus;
+    Socket s;
+
+    private messageObject messageClass;
+
+    connectionThread(String username, String IPADDR, int PORT)
+    {
+        this.username = username;
+        this.IPADDR = IPADDR;
+        this.PORT = PORT;
+        this.message = "";
+        this.recievedMessage = "";
+        this.lastSentMessage = "";
+        this.lastRecievedMessage = "";
+        this.messageClass = messageClass;
+    }
+
+    public void run()
+    {
+        try // look i just wanna check for IOException from socket but ofc buffered reader needs a try catch
+        // as well and OFC they dont work if they're not in the same scope, it is jus easier this way okay??
+        {
+
+            this.s = new Socket();
+            this.s.connect(new InetSocketAddress(IPADDR, PORT));
+            BufferedReader recvReader = new BufferedReader(new InputStreamReader(this.s.getInputStream()));
+
+            connectionSendThread sendThread = new connectionSendThread(this.s, username, messageClass);
+            sendThread.start();
+
+            while (true)
+            {
+                this.recievedMessage = recvReader.readLine();
+                Log.d("Message Recieved:", recievedMessage);
+                lastRecievedMessage = this.recievedMessage;
+            }
+
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void endConnection() {
+        try
+        {
+            this.s.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace(); // idc fuck off
+        }
+    }
+    public void sendMessage(String message)
+    {
+        this.message = message;
+    }
+
+    protected String encodeMessage(String... message)
+    {
+        try
+        {
+            String encodedString = String.join(" ", message);
+            encodedString = URLEncoder.encode(encodedString, "UTF-8");
+            return encodedString;
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            return null;
+        }
+    }
+}
+
+
+class connectionSendThread extends Thread
+{
+    Socket s;
+    String username;
+    String message;
+    String lastSentMessage;
+    PrintWriter sendWriter;
+    messageObject messageClass;
+
+    connectionSendThread(Socket _Socket, String username, messageObject messageClass)
+    {
+        this.s = _Socket;
+        this.username = username;
+        this.message = message;
+        this.messageClass = messageClass;
+    }
+
+    public void run()
+    {
+        try
+        {
+            this.sendWriter = new PrintWriter(s.getOutputStream());
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        this.sendWriter.write(encodeMessage(this.username));
+        this.sendWriter.flush();
+
+        while(true)
+        {
+            if (this.lastSentMessage != messageObject.returnMessage())
+            {
+                this.sendWriter.write(encodeMessage(messageObject.returnMessage()));
+                this.lastSentMessage = messageObject.returnMessage();
+                this.sendWriter.flush();
+            }
+        }
+    }
+    protected String encodeMessage(String... message)
+    {
+        try
+        {
+            String joinedString = String.join(" ", message);
+            String encoded = URLEncoder.encode(joinedString, "UTF-8");
+            encoded = encoded.replace('+', ' ');
+            return encoded;
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            return null;
+        }
+    }
+
+}
+
+
+class messageObject
+{
+    public static volatile String message;
+
+    public static void updateMessage(String newMessage)
+    {
+        message = newMessage;
+    }
+
+    public static String returnMessage()
+    {
+        return message;
     }
 }
