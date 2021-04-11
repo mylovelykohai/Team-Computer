6405d85036d2c201ea914df86c3bcd70b44435e2
lovelykohai <joshhunter6363@gmail.com>
2021-04-11 13:06:06 +0100

    Fixed lag

--- ApplicationWithSentiment/app/src/main/java/com/example/messagenofrag/UserProfile.java
+++ ApplicationWithSentiment/app/src/main/java/com/example/messagenofrag/UserProfile.java
@@ -20,7 +20,7 @@ public class UserProfile extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(UserProfile.this, MainActivity.class));
+                finish();
             }
         });
     }
